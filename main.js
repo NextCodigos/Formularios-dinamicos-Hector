280beab149f04d7b40c997ee771889ca601f24b1
NextCodigos <nebulanemesis376>
2024-04-29 12:45:18 +0200

    Formulario dinámico Fector Leon

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById("quantity").addEventListener("input", (event) =>{
+    let content = '';
+
+    const quantity = event.target.value;
+
+    for(let i = 0; i < quantity; i++){
+        content += `<div>
+        <label>Invitado ${i + 1}</label>
+        <input type="text" id="name[${i}]" >
+        <label>Email ${i + 1}</label>
+        <input type="email" id="email[${i}]" >
+        </div>`;
+    }
+
+    document.getElementById("divGuests").innerHTML = content;
+});
+
+document.getElementById("myForm").addEventListener("submit", (event) =>{
+    // No recargue pagina
+    event.preventDefault();
+
+    const quantity = document.getElementById("quantity").value;
+
+    const formData = new FormData(event.target);
+
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+})
