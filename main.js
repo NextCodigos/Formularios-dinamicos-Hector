e1e237c3fc47c07e3d84334d8d0becc8b0502071
NextCodigos <nebulanemesis376>
2024-04-29 12:52:41 +0200

    Formulario dinámico Fector Leon creamos json dinamico con array

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-document.getElementById("quantity").addEventListener("input", (event) =>{
-    let content = '';
+document.getElementById("quantity").addEventListener("input", (event) => {
+  let content = "";
 
-    const quantity = event.target.value;
+  const quantity = event.target.value;
 
-    for(let i = 0; i < quantity; i++){
-        content += `<div>
+  for (let i = 0; i < quantity; i++) {
+    content += `<div>
         <label>Invitado ${i + 1}</label>
         <input type="text" id="name[${i}]" >
         <label>Email ${i + 1}</label>
         <input type="email" id="email[${i}]" >
         </div>`;
-    }
+  }
 
-    document.getElementById("divGuests").innerHTML = content;
+  document.getElementById("divGuests").innerHTML = content;
 });
 
-document.getElementById("myForm").addEventListener("submit", (event) =>{
-    // No recargue pagina
-    event.preventDefault();
+document.getElementById("myForm").addEventListener("submit", (event) => {
+  // No recargue pagina
+  event.preventDefault();
 
-    const quantity = document.getElementById("quantity").value;
+  const quantity = document.getElementById("quantity").value;
 
-    const formData = new FormData(event.target);
+  const formData = new FormData(event.target);
 
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
-})
+  const data = Object.fromEntries(formData.entries());
+  //   console.log(data);
+
+  data.guest = [];
+  for (let i = 0; i < quantity; i++) {
+    const guest = {};
+    guest.name = document.getElementById(`name[${i}]`).value;
+    guest.email = document.getElementById(`email[${i}]`).value;
+
+    data.guest.push(guest);
+  }
+  console.log(data);
+});
